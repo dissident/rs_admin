5cb467a755e01e3b399055a28a24f48e450d3315
Stas Chereshkevich <svchereshkevich@domclick.ru>
2021-05-10 10:46:18 +0000

    add many things

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ require 'dotenv/load'
 
 client = Mongo::Client.new(ENV['MONGO'])
 
-collection = client['tc-exits']
+exits_collection = client['tc-exits']
+upworks_collection = client['upworks']
+
+use Rack::Auth::Basic do |username, password|
+  username == ENV['USERNAME'] and password == ENV['PASS']
+end
 
 get '/' do
   erb :index, layout: :main
@@ -13,13 +18,32 @@ end
 get '/exits' do
   limit = 20
   offset = params[:offset].to_i || 0
-  result = collection.find.skip(offset).limit(limit)
-  count = collection.count
-  erb :exits, layout: :main, locals: {
-    exits: result, count: count, offset: offset, limit: limit }
+  result = exits_collection.find({}, sort: {value: -1}).skip(offset).limit(limit)
+  count = exits_collection.count
+  erb :list, layout: :main, locals: {
+    entities: result, count: count, offset: offset, limit: limit, list_name: 'exits' }
 end
 
 get '/exits/:id' do |id|
-  exit = collection.find(_id: BSON::ObjectId(id)).first
-  erb :exit, layout: :main, locals: { exit: exit }
+  exit = exits_collection.find(_id: BSON::ObjectId(id)).first
+  erb :show, layout: :main, locals: { exit: exit }
+end
+
+get '/upworks' do
+  limit = 20
+  offset = params[:offset].to_i || 0
+  result = upworks_collection.find({}, sort: {value: -1}).skip(offset).limit(limit)
+  count = upworks_collection.count
+  
+  countries = upworks_collection.aggregate([
+    { '$match' => { 'parsed' => { '$exists' => true }}},
+    { '$group' => { '_id' => '$parsed.Country'}}
+  ])
+  erb :list, layout: :main, locals: {
+    entities: result, count: count, offset: offset, limit: limit, list_name: 'upworks' }
+end
+
+get '/upworks/:id' do |id|
+  exit = upworks_collection.find(_id: BSON::ObjectId(id)).first
+  erb :show, layout: :main, locals: { exit: exit }
 end
