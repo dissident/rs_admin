b1ac8e152e0996b66c02d872d2fa47310926fc8d
Stas Chereshkevich <svchereshkevich@domclick.ru>
2021-05-14 07:54:12 +0000

    Add sort

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ end
 get '/exits' do
   limit = 20
   offset = params[:offset].to_i || 0
-  result = exits_collection.find({}, sort: {value: -1}).skip(offset).limit(limit)
+  result = exits_collection.find({}, sort: {created_at: -1}).skip(offset).limit(limit)
   count = exits_collection.count
   erb :list, layout: :main, locals: {
     entities: result, count: count, offset: offset, limit: limit, list_name: 'exits' }
@@ -32,7 +32,7 @@ end
 get '/upworks' do
   limit = 20
   offset = params[:offset].to_i || 0
-  result = upworks_collection.find({}, sort: {value: -1}).skip(offset).limit(limit)
+  result = upworks_collection.find({}, sort: {created_at: -1}).skip(offset).limit(limit)
   count = upworks_collection.count
   
   countries = upworks_collection.aggregate([
